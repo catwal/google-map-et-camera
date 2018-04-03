0ae505b00722c178699635586293e18e9b277729
kitekate <cwallez@hotmail.fr>
2018-04-03 10:32:45 +0200

    finalisation set-location & add-place

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -14,6 +14,8 @@ export class AddPlacePage {
     lat: 40.7624324,
     lng: -73.9759827
   };
+  /*nouvelle classe pour quand localisation est faite par le user - ici par défaut false */
+  locationIsSet = false;
   
   /* le modal est une surpage de la page - un overlay */
   constructor(private modalCtrl: ModalController) { }
@@ -28,8 +30,20 @@ export class AddPlacePage {
 
   onOpenMap() {
     /* redirection vers la carte et vers un point précis de la carte */
-    const modal = this.modalCtrl.create(SetLocationPage, {location: this.location});
+    const modal = this.modalCtrl.create(SetLocationPage, 
+      /* permet ouverture pour marker ou carte avec marker du user définit par la classe isSet */
+      {location: this.location, isSet: this.locationIsSet});
     modal.present();
+    /* fermeture du modal sans ou avec données coords */
+    modal.onDidDismiss(
+      data => {
+        if (data){
+          /* appel des anciens coordonnées pour avoir les nouveaux du marker */
+          this.location = data.location;
+          this.locationIsSet = true; 
+        }
+      }
+    )
   }
 
   onTakePhoto() {
--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -14,9 +14,12 @@ export class SetLocationPage {
   /* pour gerer la gestion du modal confirme et abort j'ai besoin de view controller */
   constructor(private navParams: NavParams,
     private viewCtrl: ViewController) {
-
     /* la données va être récupérée de add-place */
     this.location = this.navParams.get('location');
+    /* injection données du marker que si user a bien cliquer sur carte isSet défini dans add-place */
+    if (this.navParams.get('isSet')) {
+      this.marker = this.location;
+    }
   }
 
   onSetMarker(event: any) {
@@ -26,12 +29,12 @@ export class SetLocationPage {
 
   onConfirm() {
     /* transmettra l'objet JS des coordonnées du marker */
-    this.viewCtrl.dismiss({location: this.marker});
+    this.viewCtrl.dismiss({ location: this.marker });
 
   }
 
   onAbort() {
     /* this.viewCtrl <=> a la vue du modal */
-this.viewCtrl.dismiss();
+    this.viewCtrl.dismiss();
   }
 }
